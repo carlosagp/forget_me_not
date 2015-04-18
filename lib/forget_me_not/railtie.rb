7e35319f46e8f985786de1c6c7ef8e7b6231c940
Carlos Garcia <carlos.garcia@carlosagp.com>
2015-04-18 16:46:36 -0600

    Added FULL namespace to ViewHelpers

--- lib/forget_me_not/railtie.rb
+++ lib/forget_me_not/railtie.rb
@@ -1,7 +1,7 @@
 module ForgetMeNot
   class Railtie < Rails::Railtie
     initializer 'forget_me_not.view_helpers' do
-      ActionView::Base.send :include, ForgetMeNot::ViewHelpers
+      ActionView::Base.send :include, ::ForgetMeNot::ViewHelpers
     end
   end
 end
